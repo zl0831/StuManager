8365e84d41d62106663a14373fd00795dfed8140
zl <17180105807@163.com>
2019-09-28 11:41:23 +0800

    修改

--- src/main/java/zl/app/StuManagerApplication.java
+++ src/main/java/zl/app/StuManagerApplication.java
@@ -2,9 +2,17 @@ package zl.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StuManagerApplication {
+public class StuManagerApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(StuManagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StuManagerApplication.class, args);
